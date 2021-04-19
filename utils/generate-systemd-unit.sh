f2ef5dfe4d20badd5a6d26ea57770a6518be66fc
Ingo Blechschmidt <iblech@speicherleck.de>
2021-04-19 06:01:50 +0200

    cleanup

--- utils/generate-systemd-unit.sh
+++ utils/generate-systemd-unit.sh
@@ -55,38 +55,3 @@ EOF
 system=$(nix-build "<nixpkgs/nixos>" -I nixos-config=$dir/configuration.nix -A system)
 
 sed -e "s+/etc/containers/+$system/etc/containers/+" -e "s+%i+agdapad+g" < $system/etc/systemd/system/container@agdapad.service
-exit
-echo $system
-
-exit
-
-bash=$(nix-build "<nixpkgs/nixos>" -I nixos-config=./container.nix -A pkgs.bash)
-iproute2=$(nix-build "<nixpkgs/nixos>" -I nixos-config=./container.nix -A pkgs.iproute2)
-
-dir="$(mktemp -d)"
-
-function cleanup {
-    rm -rf -- "$dir"
-}
-
-trap cleanup EXIT
-
-cd -- "$dir"
-mkdir -p sbin usr/lib
-touch usr/lib/os-release
-
-cat > sbin/init <<EOF
-#!$bash/bin/bash
-$iproute2/bin/ip link set dev host0 up
-$iproute2/bin/ip addr add $containerip dev host0
-$iproute2/bin/ip route add $hostip dev host0
-exec $system/init
-EOF
-
-chmod +x sbin/init
-
-systemd-nspawn -n --bind-ro=/nix/store --bind-ro=/nix/var/nix/db --bind="$home":/home --boot -M agdapad -xD "$dir" &
-
-# uff, not very nice
-sleep 1
-ifconfig ve-agdapad $hostip up
