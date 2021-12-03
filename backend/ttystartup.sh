8a6609e0a097f522748106910e3fdba11de54e92
Ingo Blechschmidt <iblech@speicherleck.de>
2021-12-04 00:52:09 +0100

    Optimize space usage using reflinks

--- backend/ttystartup.sh
+++ backend/ttystartup.sh
@@ -5,7 +5,7 @@ set -e
 cd /home/guest
 . /etc/profile
 
-cp --no-preserve=mode -nrT @out@/skeleton-home .
+cp --reflink=auto --no-preserve=mode -nrT @out@/skeleton-home .
 
 echo -en "\033]0;$1 | Agdapad\007"
 exec tmux new-session -A -s fun -- emacs hello.agda
