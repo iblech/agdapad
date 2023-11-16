8d718ced5262a8d1e6966a5016f9b443efd8aa71
Ingo Blechschmidt <iblech@speicherleck.de>
2023-11-16 22:11:39 +0100

    Get rid of polling in vnc startup

--- backend/vncinit.sh
+++ backend/vncinit.sh
@@ -10,12 +10,9 @@ add ada:1 . $cookie
 add ada/unix:1 . $cookie
 EOF
 
-Xvnc :1 -auth /home/guest/.Xauthority -desktop ada:1 -geometry 1280x768 -SecurityTypes none -UseBlacklist=off -AlwaysShared &
-
-until nc -z localhost 5901; do
-  sleep 0.1
-done
-
 export AGDAPAD_SESSION_NAME="$(grep -z ^AGDAPAD_SESSION_NAME= /proc/1/environ | cut -d= -f2-)"
 
-DISPLAY=:1 exec @out@/xstartup.sh
+# logic lifted from sx
+trap 'DISPLAY=:1 exec @out@/xstartup.sh & wait "$!"' USR1
+(trap "" USR1 && exec Xvnc :1 -auth /home/guest/.Xauthority -desktop ada:1 -geometry 1280x768 -SecurityTypes none -UseBlacklist=off -AlwaysShared) & pid=$!
+wait "$pid"
